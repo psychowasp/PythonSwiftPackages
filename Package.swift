067d88ece7076761f8803fa3df9509c6a5479c06
psychowasp <psychowaspx@gmail.com>
2023-10-05 02:08:48 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,12 @@ let package = Package(
 		
 	],
 	dependencies: [
-		.package(url: "https://github.com/KivySwiftLink/SwiftonizePlugin", branch: "master"),
+		
 		//.package(path: "../KivySwiftLink"), // required package
-		.package(url: "https://github.com/KivySwiftLink/KivySwiftLink", branch: "master"),
-
+		.package(url: "https://github.com/KivySwiftLink/KivySwiftLink", from: .init(0, 0, 0)),
+		.package(url: "https://github.com/KivySwiftLink/SwiftonizePlugin", from: .init(0, 0, 0)),
 		.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", branch: "main")
+		
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
